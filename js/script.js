e3cf097263864e6b42c344ba3f2fd311dd57aa11
Zhukovskiy <dimazhykovskiy@gmail.com>
2020-08-29 11:24:25 +0300

    Added comments to everything

--- js/script.js
+++ js/script.js
@@ -34,19 +34,19 @@ const button = document.querySelector("button"), // Переменная с кн
 
 let moviesInList = document.querySelectorAll(".promo__interactive-item"); // Псевдомассив со всеми элементами списка
 
-promo.forEach(item => item.remove());
-document.querySelector(".promo__genre").textContent = "Драма";
-document.getElementsByClassName('promo__bg')[0].style.backgroundImage = 'url("img/bg.jpg")';
+promo.forEach(item => item.remove()); // Удаляем рекламу с сайта
+document.querySelector(".promo__genre").textContent = "Драма"; // Меняем жанр фильма
+document.getElementsByClassName('promo__bg')[0].style.backgroundImage = 'url("img/bg.jpg")'; // Меняем фоновое изображание фильма на другое
 
 
-function sortingAnArray() {
+function sortingAnArray() { // Функция для сортировки массива с фильмами
     movieDB.movies.forEach((item) => item.toLowerCase());
     movieDB.movies.sort();
 }
 
 sortingAnArray();
 
-moviesInList.forEach(function (item, i) {
+moviesInList.forEach(function (item, i) { // Функция нумерующая список
     item.innerHTML = `${i + 1}. ${movieDB.movies[i]}
     <div class="delete"></div>`
 });
@@ -56,14 +56,21 @@ function addNewFilmInList() {
     const newListItem = document.createElement('li'); // Создаём новый элемент
     newListItem.innerText = newFilm.value; // Присваиваем этому элементу значение котороё ввёл пользователь
     newListItem.className = "promo__interactive-item"; // Присваиваем этому елементу класс списка
+
     movieList.insertAdjacentElement('beforeend', newListItem); //Добавляем этот эллемент в конец списка
+
     movieDB.movies.push(newFilm.value); // Добавляем значение которое ввёл пользователь в массив всех фильмов
     sortingAnArray(); // Сортируем массив
+
     moviesInList = document.querySelectorAll(".promo__interactive-item"); // Обновляем значение переменной со списком фильмов
-    moviesInList.forEach((item, i) => item.innerText = `${i + 1}. ${movieDB.movies[i]}`);
+
+    moviesInList.forEach(function (item, i) { // Функция нумерующая список
+        item.innerHTML = `${i + 1}. ${movieDB.movies[i]}
+    <div class="delete"></div>`
+    });
 }
 
-button.addEventListener('click', addNewFilmInList);
+button.addEventListener('click', addNewFilmInList); // Делаем обработчик событий для кнопки
 
 
 
